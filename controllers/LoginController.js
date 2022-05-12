243c3f4eb67776007d06aaf77adf6ba8f707c521
ocean272 <ocean272@hotmail.com>
2022-05-12 21:03:22 +0800

    added catch error in login controller

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,19 +1,22 @@
-// const loginService = require("../services/login.service");
+const loginService = require("../services/login.service");
 
-// class LoginController {
-//     async create(req, res) {
-//         const {username, email, password} = req.body;
+class LoginController {
+    async create(req, res) {
+        try{
+        const {username, email, password} = req.body;
         
-//     const { status, data, message } = await loginService.create(
-//         username,
-//         email,
-//         password,
-//       );
-//       res.status(status);
-  
-//       res.json({ message, data });
-  
-//     }
-// }
+    const { status, data, message } = await loginService.create(
+        username,
+        email,
+        password);
 
-// module.exports = LoginController;
+        res.send({status});
+        res.json({ message, data });
+
+        }catch (e) {
+            console.log(e);
+        }  
+    }
+}
+
+module.exports = LoginController;
--- models/user.js
+++ models/user.js
@@ -1,26 +1,26 @@
-// const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 
-// module.exports = function (sequelize) {
-//   class User extends Model {}
+module.exports = function (sequelize) {
+  class User extends Model {}
 
-//   User.init(
-//     {
-//       username: {
-//         type: DataTypes.STRING,
-//       },
-//       email: {
-//         type: DataTypes.STRING,
-//       },
-//       password: {
-//         type: DataTypes.STRING,
-//       },
-//     },
-//     {
-//       sequelize,
-//       tableName: "User",
-//       modelName: "user",
-//     }
-//   );
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      tableName: "User",
+      modelName: "user",
+    }
+  );
 
-//   return User;
-// };
+  return User;
+};
