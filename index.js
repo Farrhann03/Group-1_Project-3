42c243ea441ab65bf7d1a9105d3d30febe5c4f93
ocean272 <ocean272@hotmail.com>
2022-05-25 20:50:20 +0800

    added cors in route folder

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const app = require('./routes');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
-const express = require('express');
+//const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-// app.get('/', (req, res) => {
-//   res.send("Go to:  /public  /user");
-// });
+
 app.use(cors());
 app.use(bodyParser.json());
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
-
 app.get('/', (req, res) => {
-  res.render("index", { title: "HOME"});
+  res.send("Go to:  /public  /user");
 });
+
+
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "pug");
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get('/', (req, res) => {
+//   res.render("index", { title: "HOME"});
+// });
 
 
 app.listen(PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 app.use(express.json()); // Enable express to parse JSON as request body.
+const cors = require('cors');
+
+
+app.use(cors());
 
 const userRoutes = require("./user.routes");
 const publicRoutes = require("./public.routes");
