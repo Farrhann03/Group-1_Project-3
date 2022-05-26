98d2e898a9528dc8deb563941b9e9ff6af661e99
ocean272 <ocean272@hotmail.com>
2022-05-26 10:14:36 +0800

    added cors in routes folder

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const app = require('./routes');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+//const cors = require('cors');
+
+// const corsOptions = {
+//   origin: 'http://localhost:3000/',
+//   credentials: false,
+//   optionSuccessStatus: 200
+// }
 
 // app.get('/', (req, res) => {
 //   res.send("Go to:  /public  /user");
 // });
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 
 app.set("views", path.join(__dirname, "views"));
